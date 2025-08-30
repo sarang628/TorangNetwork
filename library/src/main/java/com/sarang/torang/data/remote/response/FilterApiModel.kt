8fb1e45e9e9066297c82c190f09bd7a73635bd7f
sryang <sarang628@gmail.com>
2025-08-30 18:42:44 +0800

    change parameter

--- library/src/main/java/com/sarang/torang/data/remote/response/FilterApiModel.kt
+++ library/src/main/java/com/sarang/torang/data/remote/response/FilterApiModel.kt
@@ -2,16 +2,16 @@ package com.sarang.torang.data.remote.response
 
 
 data class FilterApiModel(
-    var searchType: String = "AROUND",
-    var keyword: String? = null,
-    var distances: String? = null,
-    var prices: List<String>? = null,
-    var restaurantTypes: List<String>? = null,
-    var ratings: List<String>? = null,
-    var lat: Double? = null,
-    var lon: Double? = null,
-    var north: Double? = null,
-    var east: Double? = null,
-    var south: Double? = null,
-    var west: Double? = null
+    val searchType      : String        = "AROUND",
+    val keyword         : String?       = null,
+    val distances       : String?       = null,
+    val prices          : List<String>? = null,
+    val restaurantTypes : List<String>? = null,
+    val ratings         : List<String>? = null,
+    val latitude        : Double?       = null,
+    val longitude       : Double?       = null,
+    val northEastLat    : Double?       = null,
+    val northEastLon    : Double?       = null,
+    val southWestLat    : Double?       = null,
+    val southWestLon    : Double?       = null
 )
