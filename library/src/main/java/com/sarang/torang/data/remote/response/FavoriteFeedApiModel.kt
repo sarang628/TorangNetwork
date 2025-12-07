e51072e414b44bb3277261a50f79c396c8e319c6
sryang <sarang628@gmail.com>
2025-12-07 17:18:23 +0900

    add createDate in FeedFavoriteFeedApiModel

--- library/src/main/java/com/sarang/torang/data/remote/response/FavoriteFeedApiModel.kt
+++ library/src/main/java/com/sarang/torang/data/remote/response/FavoriteFeedApiModel.kt
@@ -1,13 +1,14 @@
 package com.sarang.torang.data.remote.response
 
 data class FavoriteFeedApiModel(
-    val reviewId: Int,
-    val favoriteId: Int,
-    val picture : Picture
+    val reviewId        : Int,
+    val favoriteId      : Int,
+    val picture         : Picture,
+    val createDate      : String
 ){
     data class Picture(
-        val pictureUrl : String,
-        val width : Int,
-        val height : Int
+        val pictureUrl  : String,
+        val width       : Int,
+        val height      : Int
     )
 }
