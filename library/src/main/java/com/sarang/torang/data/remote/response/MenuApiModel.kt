600a8c73d69e9dafc708a174e43192d6e6180fa1
sryang <sarang628@gmail.com>
2026-03-13 17:54:41 +0900

    add parentId

--- library/src/main/java/com/sarang/torang/data/remote/response/MenuApiModel.kt
+++ library/src/main/java/com/sarang/torang/data/remote/response/MenuApiModel.kt
@@ -6,9 +6,10 @@ package com.sarang.torang.data.remote.response
 data class MenuApiModel (
     val restaurantId: Int = 0,
     val rating: Float = 0f,
-    val menuId: String? = null,
+    val menuId: Int? = null,
+    val parentId: Int? = null,
     val menuName: String? = null,
-    val menuPrice: String? = null,
+    val menuPrice: Int? = null,
     val menuPicUrl: String? = null,
     val order: Int? = null,
     val children: List<MenuApiModel>? = null,
